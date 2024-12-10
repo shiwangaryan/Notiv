00a256b2342e78a90ae5d79e31e5f2ad44c69f24
arshiwang <shiwangaryan@gmail.com>
2024-12-11 00:00:30 +0530

    first commit with error in migrations/schema.ts of missing usersInAuth

---
+++ drizzle.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "drizzle-kit";
+import * as dotenv from "dotenv";
+dotenv.config({ path: ".env" });
+
+if (!process.env.DATABASE_URL) {
+  console.log("Cannot find database url in .env file");
+}
+
+export default defineConfig({
+  out: "./migrations",
+  schema: "./src/lib/supabase/schema.ts",
+  dialect: "postgresql",
+  //   driver: "pg",
+  dbCredentials: {
+    url: process.env.DATABASE_URL || "",
+  },
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import db from "@/lib/supabase/db";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
