3402d44c95b77b9bb86395c64adb49a65f562833
arshiwang <shiwangaryan@gmail.com>
2024-12-12 23:27:49 +0530

    changed card styling and added pricing card code added

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -2,15 +2,22 @@ import TitleSection from "@/components/landing-page/title-section";
 import { Button } from "@/components/ui/button";
 import Banner from "../../../public/appBanner.png";
 import Calendar from "../../../public/calendar.png";
+import Diamond from "../../../public/icons/diamond.svg";
+import CheckIcon from "../../../public/icons/check.svg";
 import React from "react";
 import Image from "next/image";
-import { CLIENTS, USERS } from "@/lib/constants";
+import { CLIENTS, PRICING_CARDS, PRICING_PLANS, USERS } from "@/lib/constants";
 import { twMerge } from "tailwind-merge";
 import clsx from "clsx";
 import CustomCard from "@/components/landing-page/custom-card";
 import { randomUUID } from "crypto";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { CardDescription, CardTitle } from "@/components/ui/card";
+import {
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const HomePage = () => {
   return (
@@ -178,15 +185,17 @@ const HomePage = () => {
             personal and professional productivity needs."
             pill="Testimonials"
           />
-          {[...Array(2)].map((arr, index) => (
+          {[...Array(2)].map((arr, arrIndex) => (
             <div
               key={randomUUID()}
               className={twMerge(
                 clsx("mt-10 flex flex-nowrap gap-6 self-start", {
-                  "flex-row-reverse": index === 1,
-                  "animate-[slide_250s_linear_infinite]": index === 0,
-                  "animate-[slide_250s_linear_infinite_reverse]": index === 1,
-                  "ml-[100vw]": index === 1,
+                  "flex-row-reverse": arrIndex === 1,
+                  "animate-[slide_300s_linear_infinite]": arrIndex === 0,
+                  "animate-[slide_300s_linear_infinite_reverse]":
+                    arrIndex === 1,
+                  "transform: translate-x-[50%]": arrIndex === 0,
+                  "ml-[100vw]": arrIndex === 1,
                 }),
                 "hover:paused"
               )}
@@ -197,33 +206,162 @@ const HomePage = () => {
                   className="w-[500px]
                 shrink-0
                 rounded-xl
+                border-2
+                border-primary-blue-600/35
                 dark:bg-gradient-to-t
-                dark:from-border dark:to-background
+                dark:from-primary-purple-700/20 dark:to-background
                 "
                   cardHeader={
                     <div className="flex items-center gap-4">
                       <Avatar>
-                        <AvatarImage src={`/avatars/${index + 1}.png`} />
+                        <AvatarImage
+                          src={
+                            arrIndex === 0
+                              ? `/avatars/${(index % 10) + 1}.png`
+                              : `/avatars/${(index % 10) + 10}.png`
+                          }
+                        />
                         <AvatarFallback>AV</AvatarFallback>
                       </Avatar>
                       <div>
                         <CardTitle className="text-foreground">
-                          {testimonial.name}
+                          {arrIndex === 0
+                            ? USERS[index % 10].name
+                            : USERS[(index % 10) + 9].name}
                         </CardTitle>
                         <CardDescription className="dark:text-washed-purple-800">
-                          {testimonial.name.toLocaleLowerCase()}
+                          {arrIndex === 0
+                            ? USERS[index % 10].name.toLocaleLowerCase()
+                            : USERS[(index % 10) + 9].name.toLocaleLowerCase()}
                         </CardDescription>
                       </div>
                     </div>
                   }
                   cardContent={
                     <p className="dark:text-washed-purple-800">
-                      {testimonial.message}
+                      {arrIndex === 0
+                        ? USERS[index % 10].message
+                        : USERS[(index % 10) + 9].message}
                     </p>
                   }
                 ></CustomCard>
               ))}
             </div>
+          ))}
+        </div>
+      </section>
+      <section
+        className="mt-20
+        px-4
+        sm:px-6
+      "
+      >
+        <TitleSection
+          title="The Perfect Plan For You"
+          subheading="Experience all the benefits of our platform. Select a plan that suits your needs and take your productivity to new heights."
+          pill="Pricing"
+        />
+        <div
+          className="flex 
+        flex-col-reverse
+        sm:flex-row
+        gap-4
+        justify-center
+        sm:items-stretch
+        items-center
+        mt-10
+        "
+        >
+          {PRICING_CARDS.map((card) => (
+            <CustomCard
+              key={card.planType}
+              className={clsx(
+                "w-[300px] rounded-2xl dark:bg-black/40 background-blur-3xl relative",
+                {
+                  "border-brand-primaryPurple/70":
+                    card.planType === PRICING_PLANS.proplan,
+                }
+              )}
+              cardHeader={
+                <CardTitle
+                  className="text-2xl
+                  font-semibold
+              "
+                >
+                  {card.planType === PRICING_PLANS.proplan && (
+                    <>
+                      <div
+                        className="hidden dark:block w-full blur-[120px] rounded-full h-32
+                        absolute
+                        bg-brand-primaryPurple/80
+                        -z-10
+                        top-0
+                      "
+                      />
+                      <Image
+                        src={Diamond}
+                        alt="Pro Plan Icon"
+                        className="absolute top-6 right-6"
+                      />
+                    </>
+                  )}
+                  {card.planType}
+                </CardTitle>
+              }
+              cardContent={
+                <CardContent className="p-0">
+                  <span
+                    className="font-normal 
+                    text-2xl
+                "
+                  >
+                    ${card.price}
+                  </span>
+                  {+card.price > 0 ? (
+                    <span className="dark:text-washed-purple-800 ml-1">
+                      /mo
+                    </span>
+                  ) : (
+                    ""
+                  )}
+                  <p className="dark:text-washed-purple-800">
+                    {card.description}
+                  </p>
+                  <Button
+                    variant="default"
+                    className="whitespace-nowrap w-full mt-4"
+                  >
+                    {card.planType === PRICING_PLANS.proplan
+                      ? "Go Pro"
+                      : "Get Started"}
+                  </Button>
+                </CardContent>
+              }
+              cardFooter={
+                <ul
+                  className="font-normal
+                  flex
+                  mb-2
+                  flex-col
+                  gap-4
+                "
+                >
+                  <small>{card.highlightFeature}</small>
+                  {card.freatures.map((feature) => (
+                    <li
+                      key={feature}
+                      className="flex
+                      items-center
+                      gap-2
+                    "
+                    >
+                      <Image src={CheckIcon} alt="Check Icon" />
+                      {feature}
+                    </li>
+                  ))}
+                </ul>
+              }
+            />
           ))}
         </div>
       </section>
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -14,99 +14,99 @@ export const CLIENTS = [
 
 export const USERS = [
   {
-    name: "Alice",
-    message:
-      "Cypress has been a game-changer for our team. With its reliable end-to-end testing, we catch bugs early, leading to faster development cycles and improved collaboration.",
-  },
-  {
     name: "Bob",
     message:
-      "I used to spend hours debugging frontend issues, but Cypress simplified everything. Now, I'm more productive, and my colleagues can trust our code thanks to Cypress.",
+      "Notiv has been a game-changer for our team. With its reliable end-to-end testing, we catch bugs early, leading to faster development cycles and improved collaboration.",
   },
   {
-    name: "Charlie",
+    name: "Alice",
     message:
-      "Cypress has transformed the way we work. Our QA and development teams are on the same page, and our productivity has skyrocketed. It's a must-have tool.",
-  },
-  {
-    name: "David",
-    message:
-      "I was skeptical at first, but Cypress exceeded my expectations. Our project timelines have improved, and collaboration between teams is seamless.",
+      "I used to spend hours debugging frontend issues, but Notiv simplified everything. Now, I'm more productive, and my colleagues can trust our code thanks to Notiv.",
   },
   {
     name: "Ella",
     message:
-      "Cypress made writing and running tests a breeze. Our team's productivity has never been higher, and we're delivering more reliable software.",
-  },
-  {
-    name: "Frank",
-    message:
-      "Thanks to Cypress, we've eliminated testing bottlenecks. Our developers and testers collaborate effortlessly, resulting in quicker releases.",
+      "Notiv has transformed the way we work. Our QA and development teams are on the same page, and our productivity has skyrocketed. It's a must-have tool.",
   },
   {
     name: "Grace",
     message:
-      "Cypress has improved our development process significantly. We now have more time for innovation, and our products are of higher quality.",
+      "I was skeptical at first, but Notiv exceeded my expectations. Our project timelines have improved, and collaboration between teams is seamless.",
   },
   {
-    name: "Hank",
+    name: "Charlie",
     message:
-      "Cypress's user-friendly interface made it easy for our non-technical team members to contribute to testing. Our workflow is much more efficient now.",
+      "Notiv made writing and running tests a breeze. Our team's productivity has never been higher, and we're delivering more reliable software.",
+  },
+  {
+    name: "Frank",
+    message:
+      "Thanks to Notiv, we've eliminated testing bottlenecks. Our developers and testers collaborate effortlessly, resulting in quicker releases.",
+  },
+  {
+    name: "David",
+    message:
+      "Notiv has improved our development process significantly. We now have more time for innovation, and our products are of higher quality.",
   },
   {
     name: "Ivy",
     message:
-      "Our team's collaboration improved immensely with Cypress. We catch issues early, leading to less friction and quicker feature deployments.",
+      "Our team's collaboration improved immensely with Notiv. We catch issues early, leading to less friction and quicker feature deployments.",
+  },
+  {
+    name: "Hank",
+    message:
+      "Notiv's user-friendly interface made it easy for our non-technical team members to contribute to testing. Our workflow is much more efficient now.",
   },
   {
     name: "Jack",
     message:
-      "Cypress's robust testing capabilities have elevated our development standards. We work more harmoniously, and our releases are more reliable.",
-  },
-  {
-    name: "Katherine",
-    message:
-      "Cypress is a lifesaver for our cross-functional teams. We're more productive, and there's a shared sense of responsibility for product quality.",
+      "Notiv's robust testing capabilities have elevated our development standards. We work more harmoniously, and our releases are more reliable.",
   },
   {
     name: "Liam",
     message:
-      "Cypress has helped us maintain high standards of quality. Our team's collaboration has improved, resulting in faster development cycles.",
-  },
-  {
-    name: "Mia",
-    message:
-      "Cypress is a powerful tool that improved our productivity and collaboration. It's now an integral part of our development process.",
+      "Notiv has helped us maintain high standards of quality. Our team's collaboration has improved, resulting in faster development cycles.",
   },
   {
     name: "Nathan",
     message:
-      "Cypress's user-friendly interface and detailed reporting have made testing a breeze. Our team's productivity is at an all-time high.",
+      "Notiv's user-friendly interface and detailed reporting have made testing a breeze. Our team's productivity is at an all-time high.",
+  },
+  {
+    name: "Katherine",
+    message:
+      "Notiv is a lifesaver for our cross-functional teams. We're more productive, and there's a shared sense of responsibility for product quality.",
+  },
+  {
+    name: "Mia",
+    message:
+      "Notiv is a powerful tool that improved our productivity and collaboration. It's now an integral part of our development process.",
   },
   {
     name: "Olivia",
     message:
-      "We saw immediate benefits in terms of productivity and collaboration after adopting Cypress. It's an essential tool for our development workflow.",
+      "We saw immediate benefits in terms of productivity and collaboration after adopting Notiv. It's an essential tool for our development workflow.",
   },
   {
-    name: "Paul",
+    name: "Priyanka",
     message:
-      "Cypress has streamlined our testing process and brought our teams closer. We're more efficient and deliver better results.",
+      "Notiv has streamlined our testing process and brought our teams closer. We're more efficient and deliver better results.",
   },
   {
     name: "Quinn",
     message:
-      "Cypress has been a game-changer for us. Our productivity and collaboration have improved significantly, leading to better software.",
+      "Notiv has been a game-changer for us. Our productivity and collaboration have improved significantly, leading to better software.",
   },
   {
     name: "Rachel",
     message:
-      "Thanks to Cypress, our testing process is now a seamless part of our development cycle. Our teams collaborate effortlessly.",
+      "Thanks to Notiv, our testing process is now a seamless part of our development cycle. Our teams collaborate effortlessly.",
   },
   {
-    name: "Sam",
+    name: "Samara",
     message:
-      "Cypress is a fantastic tool that has revolutionized our workflow. Our productivity and collaboration have reached new heights.",
+      "Notiv is a fantastic tool that has revolutionized our workflow. Our productivity and collaboration have reached new heights.",
   },
 ];
 
